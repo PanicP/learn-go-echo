859ce23c084a9b62696ec3657a2c0aa0383daaf3
PanicP <filmous2136@gmail.com>
2022-02-11 16:33:28 +0700

    WIP: test

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module learn-go-echo
 go 1.17
 
 require (
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/labstack/echo/v4 v4.6.3 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
--- src/server.go
+++ src/server.go
@@ -1,25 +1,31 @@
 package main
+
 import (
-    "github.com/labstack/echo/v4"
-    "github.com/labstack/echo/v4/middleware"
-    "net/http"
-  )
+	"learn-go-echo/src/controllers"
+
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
 func main() {
- 
- e := echo.New() 
- // Middleware
- e.Use(middleware.Logger())
- e.Use(middleware.Recover())
- 
- //CORS
- e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-  AllowOrigins: []string{"*"},
-  AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
-}))
-// Root route => handler  
-e.GET("/", func(c echo.Context) error {       
-return c.String(http.StatusOK, "Hello, World!\n")  
-})
-// Run Server
-e.Logger.Fatal(e.Start(":8000"))
+	e := echo.New()            // Middleware
+	e.Use(middleware.Logger()) // Logger
+	e.Use(middleware.Recover())
+	//CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
+	// Root route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+
+	e.POST("/price", controllers.GrabPrice) // Price endpoint
+	// Server
+	e.Logger.Fatal(e.Start(":8000"))
 }
+
+// The "godef" command is not available. Run "go install -v github.com/rogpeppe/godef@latest" to install.
