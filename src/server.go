eb7fe51f44033e8f5e285c7e4d48acb4962a468e
PanicP <filmous2136@gmail.com>
2022-02-11 15:22:27 +0700

    WIP: Change base

--- src/server.go
+++ src/server.go
@@ -1,15 +1,25 @@
 package main
 import (
-	"net/http"
-	"github.com/labstack/echo/v4"
-	"fmt"
-)
-
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4/middleware"
+    "net/http"
+  )
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	fmt.Println("test")
-	e.Logger.Fatal(e.Start(":1323"))
+ 
+ e := echo.New() 
+ // Middleware
+ e.Use(middleware.Logger())
+ e.Use(middleware.Recover())
+ 
+ //CORS
+ e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+  AllowOrigins: []string{"*"},
+  AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+}))
+// Root route => handler  
+e.GET("/", func(c echo.Context) error {       
+return c.String(http.StatusOK, "Hello, World!\n")  
+})
+// Run Server
+e.Logger.Fatal(e.Start(":8000"))
 }
